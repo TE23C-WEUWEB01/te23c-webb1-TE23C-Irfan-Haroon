9fa43155d364dbfd5cca4768696b719024a24fae
Haroon Irfan <haroon.irfan@elev.ga.ntig.se>
2025-03-13 14:26:46 +0100

    Tränat javascript, variabler, konkatenoering

---
+++ kapitel-8/intro-js/script.js
@@ -0,0 +1,17 @@
+
+// var tal1 = 12;
+// var tal2 = 34; 
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+var förnamn = "Haroon";
+var efternamn = "Irfan";
+var ålder = 18;
+var namn = förnamn + " " + efternamn; 
+// var meddelande = namn + " är " + ålder + " år gammal."
+// alert(meddelande);
+
+var hobby = "kidnapping 12 year olds."
+var kostnad = "free"
+var meddelande = namn + " likes " + hobby + " and its " + kostnad;
+alert(meddelande);
