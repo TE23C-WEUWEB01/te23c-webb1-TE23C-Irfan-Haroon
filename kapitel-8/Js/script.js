69792e4ccbce5ac425a84d922b470f42ab65e6fd
Haroon Irfan <haroon.irfan@elev.ga.ntig.se>
2025-03-28 09:49:20 +0100

    Webb teoriprov uppgift

---
+++ kapitel-8/Js/script.js
@@ -0,0 +1,24 @@
+// while (true) {
+//     var konto = prompt("Skriv ditt kontonamn");
+//     var lösenord = prompt("skriv ditt lösenord");
+
+//     if (lösenord == "1234") {
+//         document.write(`Du ${konto} är inloggad`);
+//         break;
+//     }
+//     else {
+//         document.write(`du ${konto} skrev fel lösenord`);
+//     }
+// }
+
+
+// for (let index = 20; index < 31; index++) {
+//     document.writeln(`tal: ${index} <br>`);
+    
+// }
+
+let val = true;
+
+while (val) {
+    val = confirm("Klicka Ok för att fortsätta!");
+}
